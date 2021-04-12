9621fde48471924eebaf8bc0276880bdb8945832
John Bielejeski <jmbielejeski@gmail.com>
2021-04-12 08:21:04 -0700

    added basic Home Page

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="container">
+      <h2>Welcome!</h2>
+      <p>To get started create a team or use the code your captain gave you to join their team</p>
+      <button>Create Team</button>
+      <button>Join Team</button>
+    </div>
+  );
+}
+
+export default HomePage;
