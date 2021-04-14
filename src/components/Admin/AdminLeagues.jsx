f23e01f7d6a2439ba3cc482ba6e753515cb0126e
Zack Werner <allthelight044@gmail.com>
2021-04-13 20:40:41 -0500

    created static data for the leagues view, wshould be able to add real data later on.

--- src/components/Admin/AdminLeagues.jsx
+++ src/components/Admin/AdminLeagues.jsx
@@ -1,10 +1,114 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import './AdminCreate.css';
+import { Grid } from '@material-ui/core';
 
 function AdminLeagues() {
   return (
-    <div className="container">
+    <Grid
+      container
+      item
+      xs={12}
+      direction="row"
+      justify="center"
+      alignItems="center"
+    >
+
+      <Grid  
+      container
+      item
+      xs={12}
+      direction="row"
+      justify="center"
+      alignItems="center"
+      >
       <h1>Leagues</h1>
-    </div>
+      </Grid>
+
+      <Grid
+      container
+      item
+      xs={12}
+      direction="row"
+      justify="center"
+      alignItems="center"
+      >
+      <Button variant="outlined" color="primary">
+            Create a League
+          </Button>
+      </Grid>
+
+    <Grid 
+    container
+    item
+    xs={12}
+    direction="row"
+    //space-around
+    justify="center"
+    alignItems="center"
+    >
+      <Grid item xs={2} >
+        <h2>League</h2>
+      </Grid>
+
+      <Grid item xs={2} >
+        <h2>Start Date</h2>
+      </Grid>
+
+      <Grid item xs={2} >
+        <h2>End Date</h2>
+      </Grid>
+
+      <Grid item xs={2} >
+        <h2>Status</h2>
+      </Grid>
+    </Grid>
+
+      <Grid 
+      container
+      item
+      xs={12}
+      direction="row"
+      // space-around
+      justify="center"
+      alignItems="center"
+      >
+        <Grid item xs={2}>
+          {/* Season League */}
+          <h3>Spring League 2021</h3>
+        </Grid>
+
+        <Grid item xs={2}>
+          {/* Start Date */}
+          <p>05/03/2021</p>
+        </Grid>
+
+        <Grid item xs={2} >
+          {/* End Date */}
+          <p>05/03/2021</p>
+        </Grid>
+
+        <Grid item xs={2} >
+          {/* Status */}
+          <p>In Progress</p>
+        </Grid>
+
+        <Grid item={2}>
+          <Button variant="outlined" color="primary">
+              Edit
+          </Button>
+        </Grid>
+
+        <Grid item={2}>
+          <Button variant="outlined" color="secondary">
+              Delete
+          </Button>
+        </Grid>
+
+      </Grid>
+
+    </Grid>
   );
 }
 
