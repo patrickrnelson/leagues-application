ee2acab15f1a9ed13f4c83fd258fd36e8fcb95d1
Johnny Goth <Jgoth23@gmail.com>
2021-04-13 23:42:36 -0500

    started table

--- src/components/Admin/AdminTeams.jsx
+++ src/components/Admin/AdminTeams.jsx
@@ -5,12 +5,28 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
+// import Table from '@material-ui/core/Table';
+// import TableBody from '@material-ui/core/TableBody';
+// import TableCell from '@material-ui/core/TableCell';
+// import TableContainer from '@material-ui/core/TableContainer';
+// import TableHead from '@material-ui/core/TableHead';
+// import TableRow from '@material-ui/core/TableRow';
+// import Paper from '@material-ui/core/Paper';
+
+// const useStyles = makeStyles({
+//   table: {
+//     minWidth: 650,
+//   },
+// });
 
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(1),
     minWidth: 200
+  },
+  table: {
+    minWidth: 650,
   }
 }))
 
@@ -44,15 +60,50 @@ function AdminTeams() {
           value={value}
           onChange={handleChange}
         >
-          <MenuItem value={'Team1'}>Team1</MenuItem>
-          <MenuItem value={'Team2'}>Team2</MenuItem>
-          <MenuItem value={'Team3'}>Team3</MenuItem>
+          <MenuItem value={'League1'}>Fall League 2020</MenuItem>
+          <MenuItem value={'League2'}>Spring League 2021</MenuItem>
+          <MenuItem value={'League3'}>Fall League 2021</MenuItem>
           <MenuItem value={'Team4'}>Team4</MenuItem>
           <MenuItem value={'Team5'}>Team5</MenuItem>
         </Select>
       </FormControl>
       </Grid>
+      {/* <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow></TableRow>
+        </TableHead>
+      </Table>
+      </TableContainer> */}
+      <table>
+  <tr>
+    <th>Climber</th>
+    <th>Color</th>
+    <th>Location</th>
+    <th>Difficulty</th>
+    <th>Score</th>
+    <th>Attempts</th>
+    <th>Dates</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Red</td>
+    <td>Slab</td>
+    <td>V6</td>
+    <td>8</td>
+    <td>2</td>
+    <td>4/14/2021</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  
+</table>
+      
   </Grid>
+ 
   );
 }
 
