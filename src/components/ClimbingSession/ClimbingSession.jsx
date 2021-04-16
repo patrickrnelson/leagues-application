ea24f35933b12bfc821aba49eb5f4e99b05b95d1
John Bielejeski <jmbielejeski@gmail.com>
2021-04-16 08:27:02 -0700

    setup for loop to go over climbs for the week and put submitted climbs into an array

--- src/components/ClimbingSession/ClimbingSession.jsx
+++ src/components/ClimbingSession/ClimbingSession.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import moment from 'moment';
 
 import Header from '../Header/Header'
 import './ClimbingSession.css'
@@ -11,6 +12,71 @@ function ClimbingSession() {
 
   const user = useSelector(store => store.user)
   const climbs = useSelector(store => store.climbs)
+  const conditionalData = useSelector(store => store.conditional);
+
+  // grab our start date and end date
+  let from = new Date(conditionalData[0].start).getTime();
+  let to = new Date(conditionalData[0].end).getTime();
+  let week = 604800000;
+  let day = 86400000;
+  let allWeeks = [];
+  let current =  1;
+  // determine the number of weeks in the league
+  let weeks = (to-from)/day/7
+
+  // loop over weeks array to add each end of  week date to allWeeks array
+  for (let i = 0; i < weeks; i++){
+    allWeeks.push(new Date(from += week).toLocaleDateString())
+  }
+
+  // Loop to determine the index of the week so we can check if today is before the end of that week
+  let weekCalc = 0;
+  for (let i = 0; i < allWeeks.length; i++) {
+    if (moment().isSameOrBefore(allWeeks[i])) {
+    weekCalc = i;
+    break;
+    }
+  }
+
+  console.log('all weeks at week calc', allWeeks[weekCalc]);
+
+  // check if week 1 - if it is week 1 check if before week end date and after start date of league
+  // else check if before end of week and after end of previous week
+  let weekOneClimbs = [];
+  for(let climb of climbs) {
+    if (moment(climb.climbDate).isBefore(allWeeks[weekCalc]) && moment(climb.climbDate).isAfter(conditionalData[0].start)) {
+      console.log('current week', climb.climbDate)
+      if (climb.isSubmitted === true) {
+        weekOneClimbs.push(climb.level);
+      }
+    }
+  }
+  
+  console.log('weekOneClimbs', weekOneClimbs);
+
+
+  // week 1 check if after start date
+  // every other week check if after end of previous week
+
+  // handicap = average difficulty of submitted climbs
+  // TBD for week 1?
+
+  /**
+   * 
+   * Figure out if the climb is in week 1
+    if(date is between 4/12 and 4/18) {
+      if(climb is submitted) {
+        handicap = AVG(climb 1, climb 2, climb 3)
+      }
+    }
+    climbDifference = climb 1 - handicap
+    if(climbDifference = 0) {
+      climbOneScore = 5
+    }
+   * 
+   *  */ 
+
+
 
   return (
     <div className="container">
@@ -32,7 +98,7 @@ function ClimbingSession() {
           </thead>
           <tbody>
             {climbs.map((climb) => climb.userId === user.id ? 
-              <tr>  
+              <tr key={climb.climbId}>  
               <td> {climb.color} </td>
               <td> {climb.locationName} </td>
               <td> {climb.attempts} </td>
