3284c08ea448544110ebae2be685e71029b4d2cb
patrickrnelson <patrickrnelson1@gmail.com>
2021-04-15 20:44:42 -0500

    trying to figure out scoring. putting it off for now.

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -8,17 +8,56 @@ function Leaderboard() {
 
   const leagues = useSelector(store => store.leaguesReducer);
   const leagueTeams = useSelector(store => store.leagueTeamReducer);
+  const climbs = useSelector(store => store.climbs)
   const user = useSelector(store => store.user);
 
   const [currentWeek, setCurrentWeek] = useState('')
   const [currentLeague, setCurrentLeague] = useState('Cullen')
 
-  const weeks = ['Week 1', 'Week 2', , 'Week 3', 'Week 4', 'Week 5', , 'Week 6', 'Week 7']
+  // const [startDate, setStartDate] = useState('')
+  // const [endDate, setEndDate] = useState('')
 
   useEffect(() => {
-    // whatLeague();
+    findLeagueDates();
+    findClimbDates();
     console.log('leagues', leagues);
   }, [])
+
+  let startDate = ''
+  let endDate = ''
+
+  // define dates of the current league
+  const findLeagueDates = () => {
+    for(let league of leagues) {
+      if(league.name === currentLeague) {
+        startDate = league.start
+        endDate = league.end
+        // setStartDate(league.start);
+        // setEndDate(league.end);
+      }
+    }
+    console.log('start date', startDate);
+    console.log('end date', endDate);
+  } // end findLeagueDates
+  
+  
+  // find the user's climb dates
+  const findClimbDates = () => {
+    let climbDates = [];
+    for(let climb of climbs) {
+      if(climb.userId === user.id && climb.isSubmitted === true){
+        climbDates.push(climb.climbDate)
+      }
+    }       
+    console.log(climbDates);
+
+    // let weekOneClimbs = [];
+    // for(let date of climbDates) {
+    //   if(date <  )
+    // }
+  } // end findClimbDates
+
+  const weeks = ['Week 1', 'Week 2', , 'Week 3', 'Week 4', 'Week 5', , 'Week 6', 'Week 7']
 
   // This can define the current league eventually
   // const whatLeague = () => {
