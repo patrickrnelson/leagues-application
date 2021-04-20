ed2ba73c181ff1faf369d480d63d45fafd0630b7
Alvin Wong <wong.alvin13@gmail.com>
2021-04-20 12:44:17 -0500

    hamburger header styled

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory, Link } from 'react-router-dom';
 
 // Material UI imports
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import Grid from '@material-ui/core/Grid';
 import MenuIcon from '@material-ui/icons/Menu';
-import CloseIcon from '@material-ui/icons/Close';
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
+import HomeIcon from '@material-ui/icons/Home';
+import PeopleIcon from '@material-ui/icons/People';
+import ScoreIcon from '@material-ui/icons/Score';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ListIcon from '@material-ui/icons/List';
+import InfoIcon from '@material-ui/icons/Info';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 import './Header.css';
-import LogOutButton from '../LogOutButton/LogOutButton';
 import logo from '../Images/notn.png';
+import { ClassSharp } from '@material-ui/icons';
 
 // Material UI styles
 const useStyles = makeStyles({
@@ -22,32 +31,32 @@ const useStyles = makeStyles({
     flexGrow: 1,
   },
   list: {
-    width: "160px",
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: '#ffffff',
+    width: "180px",
   },
-  logOut: { 
-    textAlign: 'right',
-    marginTop: '120px', 
-    color: '#ad0c0c'
-  }, 
-  btn: {
-    width: '130px',
-    height: '40px',
+  fullList: {
+    width: 'auto',
+  },
+  icon: {
+    color: '#ffffff',
   },
   hamburger: {
-    color: 'white',
+    color: '#ffffff',
     paddingTop: 0.1,
   },
   title: {
-    color: 'white',
+    color: '#ffffff',
     paddingTop: 4,
+  },
+  paper: {
+    background: '#535353',
   },
 });
 
 function Header () {
   const history = useHistory();
   const classes = useStyles();
+  const dispatch = useDispatch();
 
   // local state for drawer (hamburger menu)
   const [drawer, setDrawer] = useState(false);
@@ -56,57 +65,52 @@ function Header () {
     setDrawer(!drawer);
   };
 
+
+
   const list = (
-    <div style={{ padding: 20 }}>
-      <Grid
-        container
-        spacing={3}
-        direction="column"
-        justify="flex-start"
-        alignItems="flex-start"
+    <div className={classes.fullList}>
+      <List
         className={classes.list}
         onClick={toggleDrawer}
         onKeyDown={toggleDrawer}
         role="presentation">
-        <Grid item xs={6} style={{alignItems: "right"}}>
+        {/* <ListItem item xs={6} style={{alignItems: "right"}}>
           <IconButton onClick={toggleDrawer}>
-            <CloseIcon />
+            <CloseIcon className={classes.close} />
           </IconButton>
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant='outlined' color="secondary" className={classes.btn} style={{ border: '2px solid' }}>
-            <Link onClick={() => history.push('/home')}>Home</Link>
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant='outlined' color="secondary" className={classes.btn} style={{ border: '2px solid' }}>
-            <Link onClick={() => history.push('/team')}>Team</Link>
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant='outlined' color="secondary" className={classes.btn} style={{ border: '2px solid' }}>
-            <Link onClick={() => history.push('/leaderboard')}>Leaderboard</Link>
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant='outlined' color="secondary" className={classes.btn} style={{ border: '2px solid' }}>
-            <Link onClick={() => history.push('/climberProfile')}>Profile</Link>
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant='outlined' color="secondary" className={classes.btn} style={{ border: '2px solid' }}>
-            <Link onClick={() => history.push('/rules')}>Rules</Link>
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant='outlined' color="secondary" className={classes.btn} style={{ border: '2px solid' }}>
-            <Link onClick={() => history.push('/about')}>About</Link>
-          </Button>
-        </Grid>
-        <Grid>
-          <LogOutButton />
-        </Grid>  
-      </Grid>
+        </ListItem> */}
+        <ListItem onClick={() => history.push('/home')}>
+          <ListItemIcon className={classes.icon}>{<HomeIcon />}</ListItemIcon>
+          <ListItemText>Home</ListItemText>
+        </ListItem>
+        <ListItem onClick={() => history.push('/climberProfile')}>
+          <ListItemIcon className={classes.icon}>{<AccountCircleIcon />}</ListItemIcon>
+          <ListItemText>Profile</ListItemText>
+        </ListItem>
+        <ListItem onClick={() => history.push('/team')}>
+          <ListItemIcon className={classes.icon}>{<PeopleIcon />}</ListItemIcon>
+          <ListItemText>Team</ListItemText>
+        </ListItem>
+        <ListItem onClick={() => history.push('/leaderboard')}>
+          <ListItemIcon className={classes.icon}>{<ScoreIcon />}</ListItemIcon>
+          <ListItemText>Leaderboard</ListItemText>
+        </ListItem>
+        <ListItem onClick={() => history.push('/rules')}>
+          <ListItemIcon className={classes.icon}>{<ListIcon />}</ListItemIcon>
+          <ListItemText>Rules</ListItemText>
+        </ListItem>
+        <ListItem onClick={() => history.push('/about')}>
+          <ListItemIcon className={classes.icon}>{<InfoIcon />}</ListItemIcon>
+          <ListItemText>About</ListItemText>
+        </ListItem>
+      </List>
+      <Divider />
+      <List className={classes.list}>
+        <ListItem onClick={() => dispatch({ type: 'LOGOUT' })}>
+          <ListItemIcon className={classes.icon}>{<ExitToAppIcon />}</ListItemIcon>
+          <ListItemText>Logout</ListItemText>
+        </ListItem>
+      </List>
     </div>
   ) // end list
 
@@ -120,7 +124,7 @@ function Header () {
       <IconButton className={classes.hamburger} onClick={toggleDrawer} >
         <MenuIcon fontSize="large" />
       </IconButton>
-      <Drawer anchor="right" open={drawer} onClose={toggleDrawer} className={classes.menu}>
+      <Drawer anchor="right" open={drawer} onClose={toggleDrawer} classes={{ paper: classes.paper }}>
         {list}
       </Drawer>
       {/* End drawer section */}
